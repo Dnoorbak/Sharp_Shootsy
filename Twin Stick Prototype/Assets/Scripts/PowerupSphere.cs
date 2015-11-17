1015d370aadcfde7e4c8fedce477c7171baa33b1
scottyscooter <reptilenut_77@yahoo.com>
2015-11-16 20:19:39 -0500

    Added powerup balls
    Added 3 balls that circle the players ship. These were added to both
    player 1 and 2.

---
+++ Twin Stick Prototype/Assets/Scripts/PowerupSphere.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupSphere : MonoBehaviour
+{
+    Transform revolveAround; //object for powerup sphere to revolve around
+    float rotationSpeed = 360f; //how many degrees to rotate a second
+
+    void Start()
+    {
+        revolveAround = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(revolveAround.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
