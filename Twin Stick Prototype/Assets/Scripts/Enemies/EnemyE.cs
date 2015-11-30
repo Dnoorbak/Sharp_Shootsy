9dd30f75b2211c8bf1ba663bea2c92951af11ad7
Kuefler <rlk89@rlk89.com>
2015-11-30 16:45:00 -0500

    Added enemy Goodness
    Improved one enemy, Added 2 I think, and added a manager for the
    enemies.

--- Twin Stick Prototype/Assets/Scripts/Enemies/EnemyE.cs
+++ Twin Stick Prototype/Assets/Scripts/Enemies/EnemyE.cs
@@ -1,18 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class EnemyD : BaseEnemy {
+public class EnemyE : BaseEnemy {
 	public float speed = 0.04f;
 	public bool _____________________;
 	public float stateOffset1;
 	public float stateOffset2;
-	public float stateOffset3;
 	// Use this for initialization
 	void Start() {
-		stateOffset1 = Random.Range (-4f, 4f);
-
-		stateOffset2 = Random.Range (-4f, 4f);
-		stateOffset3 = Random.Range (-4f, 4f);
+		stateOffset1 = Random.Range (-4, 4);
+		stateOffset2 = Random.Range (-4, 4);
 	}
 	
 	// Update is called once per frame
@@ -23,16 +20,13 @@ public class EnemyD : BaseEnemy {
 			                                              this.player.transform.position, speed);
 			float sinFactor = 5f;
 			float sigMagnatude = 0.2f;
-			this.transform.position += new Vector3(
-				Mathf.Clamp(Mathf.Tan((stateOffset1 + Time.time) * sinFactor),-4,4) * sigMagnatude,
-				Mathf.Clamp(Mathf.Tan((stateOffset3 + Time.time) * sinFactor),-4, 4) * sigMagnatude,
-				0);
+			this.transform.position += new Vector3(Mathf.Sin((stateOffset1 + Time.time) * sinFactor) * sigMagnatude, Mathf.Cos((stateOffset1 +Time.time) * sinFactor) * sigMagnatude, 0);
 			float sinFactor2 = -2f;
 			float sigMagnatude2 = 0.5f;
 			this.transform.position += new Vector3(Mathf.Sin((stateOffset2 + Time.time) * sinFactor) * sigMagnatude, Mathf.Cos((stateOffset2 +Time.time) * sinFactor) * sigMagnatude, 0);
-			
-			
-			
+
+		
+		
 		}
 	}
 }
--- Twin Stick Prototype/Assets/Scripts/enemyHelper.cs
+++ Twin Stick Prototype/Assets/Scripts/enemyHelper.cs
@@ -1,19 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class enemyHelper : MonoBehaviour {
-    public int rate = 1;
-    public int level = 0;
-	public GameObject[] prefabs; 
+   // public int rate = 1;
+   // public int level = 0;
+	public GameObject[] prefabs;
+	public string [] EnemyTypeList;
+	public string [] eventList;
+	public float distanceAwayFromPlayer;
+	public float MapDistance = 50;
+	public GameObject player;
 	// Use this for initialization
-
+	public bool ________;
+	public int Level =1;
+	public int current = 0;
+	public bool doneWithLast=true;
 
 	void Start () {
-        InvokeRepeating("Spawn", 5, 1);
+        //InvokeRepeating("Spawn", 5, 1);
+		//remove all so i can count
+		foreach (Transform child in this.transform) {
+			Destroy(child.gameObject);
+		}
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (doneWithLast) {
+			doneWithLast = false;
+			string currentString = string.Copy (eventList [current]);
+			if (currentString.Contains ("Spawn")) {
+				currentString = currentString.Replace ("Spawn", "");
+		
+
+				if (currentString.Contains ("Random")) {
+
+					currentString = currentString.Replace ("Random", "");
+					int count = int.Parse (currentString);
+					Spawn (1, 0, EnemyTypeList.Length);
+					//int count =  int.Parse( eventList[current]);
+				}
+	
+		
+				else if (currentString.Contains ("Enemy")) {
+					for (int i = 0; i <  EnemyTypeList.Length; i++) {
+					
+
+						if (currentString.Contains (EnemyTypeList [i])) {
+
+						
+							currentString = currentString.Replace (EnemyTypeList [i], "");
+							int count = int.Parse (currentString);
+							Spawn (count, i, i);
+						}
+					}
+					//int count =  int.Parse( eventList[current]);
+				}
+	
+
+			}//end of "spawn"
+			else if (currentString.Contains ("UpLevel")) {
+				currentString = currentString.Replace ("UpLevel", "");
+				Level +=1;
+			}
+			else if (currentString.Contains ("GoTo")) {
+				currentString = currentString.Replace ("GoTo", "");
+				current = int.Parse (currentString) -1;
+		
+				
+			}
+			else 
+			{
+				
+				print ("Error!!! you didnt type something correctllly" );
+			}
+
+
+
+		} else {
+			if(transform.childCount <1)
+			{
+				doneWithLast = true;
+				current ++;
+
+
+			}
+
+
+		}
 	    //if ((int)Random.Range (0, 100) == 1) {
 	    //		GameObject tempObject =	GameObject.Instantiate(prefabs[(int)Random.Range(0,4)]);
 	    //		tempObject.GetComponent<BaseEnemy>().gameObject.transform.position  = new Vector3(Random.Range(-50,50), Random.Range(-50,50), 0);
@@ -21,19 +97,38 @@ public class enemyHelper : MonoBehaviour {
 	
 	}
 
-    void Spawn()
+	//public int TypeToIndex
+
+
+	//new Rect(new Vector2(-50, -50, 100,100)).Contains(newSpot))
+
+    void Spawn(int rate, int startType, int endtype)
     {
         for (int i = 0; i < rate; i++)
         {
-            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), 0), transform.rotation);
-            //tempObject.GetComponent<BaseEnemy>().gameObject.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), 0);
-        }
-        level++;
+			Vector3 newSpot;
+			do {
 
-        if (level % 30 == 0)
+				newSpot = new Vector3(Random.Range(-MapDistance, MapDistance), Random.Range(-MapDistance, MapDistance), 0);
+				                      } while (
+					(Vector3.Distance(newSpot, player.transform.position)<distanceAwayFromPlayer)
+					&&
+					//(Rect.MinMaxRect(-MapDistance, -MapDistance, MapDistance,MapDistance).Contains(newSpot))
+					true
+
+					);
+				                      GameObject temp = (GameObject)Instantiate(prefabs[Random.Range(startType, endtype+1)], newSpot, transform.rotation);
+			temp.GetComponent<BaseEnemy>().UpLevel(Level);
+			temp.transform.parent = this.gameObject.transform;
+			//tempObject.GetComponent<BaseEnemy>().gameObject.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), 0);
+        }
+
+       // level++;
+
+       /* if (level % 30 == 0)
         {
             rate++;
             print("rate: " + rate);
-        }
+        }*/
     }
 }
