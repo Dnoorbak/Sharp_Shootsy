01d3da696e654cc30b31053690713e6e37ed482f
scottyscooter <reptilenut_77@yahoo.com>
2015-11-30 23:06:22 -0500

    Add xbox controller support and an error log folder with a bug I found
    Now the game looks to see if there is an xbox controller hooked up and
    uses vibration to alert the player of being hit. Might need to tweak the
    vibration setting depending on how you guys feel about the time and
    intensity of it.
    
    Also, added an error log file that contains one bug that was found while
    testing the game. I will next add the high score keeper file (maybe
    Geoff can add a high score screen after player dies?) and a gauge that
    works off the number of enemies shot.

--- Twin Stick Prototype/Assets/Scripts/PlayerControl.cs
+++ Twin Stick Prototype/Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using XInputDotNetPure;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -18,6 +19,16 @@ public class PlayerControl : MonoBehaviour
     public GameObject damage;
     private bool alive;
 
+    //Controller variables
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    float angle = 0.0f; //current angle ship is facing
+    float previousAngle = 0.0f; //previous angle ship was facing
+    public float leftVibration = 0.0f;
+    public float rightVibration = 0.0f;
+
     void Start()
     {
 		rb = GetComponent<Rigidbody> ();
@@ -28,11 +39,14 @@ public class PlayerControl : MonoBehaviour
         //speed = (1 / FollowCam.S.timeMag) *100;
 
     }
+
     void OnTriggerEnter(Collider collision)
     {
         if (alive) {
             if (collision.gameObject.tag == "Enemy" && invCountdown <= 0)
             {
+                HitVibration(true);
+
                 FollowCam.S.GetComponent<UnityStandardAssets.ImageEffects.MotionBlur>().enabled = true;
                 //FollowCam.S.GetComponent<UnityStandardAssets.ImageEffects.ColorCorrectionCurves>().enabled = true;
                 lives--;
@@ -52,40 +66,54 @@ public class PlayerControl : MonoBehaviour
         if (invCountdown > 0)
         {
             invCountdown -= Time.deltaTime;
-
         }
         else
         {
             FollowCam.S.GetComponent<UnityStandardAssets.ImageEffects.MotionBlur>().enabled = false;
+
+            //turn vibration of controller off
+            HitVibration(false);
+
             //FollowCam.S.GetComponent<UnityStandardAssets.ImageEffects.ColorCorrectionCurves>().enabled = false;
         }
+
+        CheckControllerConnections();
     }
 
     void FixedUpdate()
     {
-        if (alive)
-        {
-
-            Vector3 rotation;
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
-
-            rb.AddForce(movement * speed);
-
-            if ((Input.GetAxis("Horizontal2") > 0.2 || (Input.GetAxis("Vertical2") > 0.2) || (Input.GetAxis("Horizontal2") < -0.2) || (Input.GetAxis("Vertical2") < -0.2)))
+        if (state.IsConnected) { //xbox controller is connected
+            if (alive)
             {
-                direction = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
-
-                foreach (Launcher s in shots)
-                {
-                    s.Shoot();
-                }
+                PlayerMove();
+                PlayerRotate();
+                Fire();
             }
-            //transform.up = direction;
-            rotation = Vector3.Lerp(transform.up, direction, turnSpeed);
-            transform.up = rotation;
+        }
+        else { //non-xbox controls (ie, no vibration of controller)
+            if (alive)
+            {
+                Vector3 rotation;
+                float moveHorizontal = Input.GetAxis("Horizontal");
+                float moveVertical = Input.GetAxis("Vertical");
+
+                Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+
+                rb.AddForce(movement * speed);
+
+                if ((Input.GetAxis("Horizontal2") > 0.2 || (Input.GetAxis("Vertical2") > 0.2) || (Input.GetAxis("Horizontal2") < -0.2) || (Input.GetAxis("Vertical2") < -0.2)))
+                {
+                    direction = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+
+                    foreach (Launcher s in shots)
+                    {
+                        s.Shoot();
+                    }
+                }
+                //transform.up = direction;
+                rotation = Vector3.Lerp(transform.up, direction, turnSpeed);
+                transform.up = rotation;
+            }
         }
     }
 
@@ -93,7 +121,87 @@ public class PlayerControl : MonoBehaviour
     {
         Instantiate(explosion, transform.position, transform.rotation);
         alive = false;
+        HitVibration(false);
         //GetComponent<BoxCollider>().enabled = false;
         GameObject.DestroyObject(this.gameObject);
+    }
+
+    void CheckControllerConnections()
+    {
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
+
+    void PlayerMove()
+    {
+        float moveHorizontal = state.ThumbSticks.Left.X;
+        float moveVertical = state.ThumbSticks.Left.Y;
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void PlayerRotate()
+    {
+        // Set a deadzone for the right stick
+        if ((state.ThumbSticks.Right.X > -0.2 && state.ThumbSticks.Right.X < 0.2) &&
+            (state.ThumbSticks.Right.Y > -0.2 && state.ThumbSticks.Right.Y < 0.2))
+        {
+            //if the stick wasn't moved enough, the current angle is whatever the angle was previously
+            angle = previousAngle; 
+        }
+        else
+        {
+            //set our previous angle and then update our current angle
+            previousAngle = angle;
+            angle = Mathf.Atan2(state.ThumbSticks.Right.Y, state.ThumbSticks.Right.X) * Mathf.Rad2Deg;
+        }
+
+        //update the rotation of the player
+        transform.rotation = Quaternion.AngleAxis(90.0f - angle, Vector3.back);
+    }
+
+    void HitVibration(bool shouldVibrate)
+    {
+        if (shouldVibrate)
+        {
+            // Set vibration
+            leftVibration = rightVibration = 100.0f;
+            GamePad.SetVibration(playerIndex, leftVibration, rightVibration);
+        }
+        else
+        {
+            //turn vibration of controller off
+            leftVibration = rightVibration = 0.0f;
+            GamePad.SetVibration(playerIndex, leftVibration, rightVibration);
+        }
+    }
+
+    void Fire()
+    {
+        direction = new Vector3(state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y, 0);
+
+        foreach (Launcher s in shots)
+        {
+            s.Shoot();
+        }
     }
 }
