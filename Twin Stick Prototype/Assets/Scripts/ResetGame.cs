207f7866db04c1748b3d4342dcc2bf141139dbcb
scottyscooter <reptilenut_77@yahoo.com>
2015-12-09 20:16:22 -0500

    Added ability to reset game level
    It is what it says

---
+++ Twin Stick Prototype/Assets/Scripts/ResetGame.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using XInputDotNetPure;
+
+public class ResetGame : MonoBehaviour {
+
+    //Controller variables
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckControllerConnections();
+
+        if (state.Buttons.Start == ButtonState.Pressed)
+        {
+            Application.LoadLevel("Test_0");
+        }
+	}
+
+    void CheckControllerConnections ()
+    {
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex) i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
+}
